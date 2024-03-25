d338778ba1241b25959acd51332b2e6e655d3d41
Sofie Cai <sofie.cai@ucll.be>
2024-03-25 17:58:18 +0100

    css

--- src/pages/mainpage.tsx
+++ src/pages/mainpage.tsx
@@ -1,7 +1,7 @@
 
 function ProjectsPage() {
     return <>
-        <main className="  bg-white">
+        <main className=" * p-0 * border-b-0">
             <h1 className=" font-bold text-center  border-cyan-400 text-black">Welkom op de Hoofdpagina van Band-aid!</h1>
         </main>
     </>
