d685ce004745dae3dea221c82554fc5072670491
Sofie Cai <sofie.cai@ucll.be>
2024-03-26 12:42:24 +0100

    navbar

--- src/pages/mainpage.tsx
+++ src/pages/mainpage.tsx
@@ -1,9 +1,16 @@
+import NavBar from '../components/NavBar';
 
 function ProjectsPage() {
-    return <>
-        <main className=" * p-0 * border-b-0">
-            <h1 className=" font-bold text-center  border-cyan-400 text-black">Welkom op de Hoofdpagina van Band-aid!</h1>
-        </main>
-    </>
+    return (
+        <>
+            <main className="p-0 border-b-0">
+                <header className="bg-base-100 mb-40 shadow-xl rounded-box" >
+                    <NavBar />
+                </header>
+                <h1 className="font-bold text-center border-cyan-400 text-black">Welkom op de Hoofdpagina van Band-aid!</h1>
+            </main>
+        </>
+    );
 }
+
 export default ProjectsPage;
