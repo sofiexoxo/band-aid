56fc166dc402288cb0e7e526e351d9e615180054
Sofie Cai <sofie.cai@ucll.be>
2024-04-19 20:16:03 +0200

    delete

--- src/lib/api.ts
+++
@@ -1,6 +0,0 @@
-import createClient from "openapi-fetch";
-import type { paths } from "./api/generated";
-
-const client = createClient<paths>({ baseUrl: "http://127.0.0.1:8000/" });
-
-const { data, error } = await client.GET("/");
