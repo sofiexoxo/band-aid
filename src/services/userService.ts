98790c5737e03cb716ba07cb43e08c2df672ce16
Sofie Cai <sofie.cai@ucll.be>
2024-04-25 15:02:07 +0200

    install package

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,13 +3,25 @@ import type { paths } from "../lib/api/v1";
 
 const client = createClient<paths>({ baseUrl: "/" });
 
-const getBands = await client.GET("/bands");
+const getBands = await client.GET("/api/bands");
 
-
-const postRegister = await client.POST("/register", {
+const postToken = await client.POST("/api/token", {
+    params: {
+        query: { dsn: "dsn" },
+    },
+    body: {
+        grant_type: "",
+        username: "username",
+        password: "password",
+        scope: "",
+        client_id: "",
+        client_secret: "",
+    }
+});
+const postRegister = await client.POST("/api/register", (req, res) {
     body: {
         email: "string",
-        password: "string",
+            password: "string",
     },
 });
 
