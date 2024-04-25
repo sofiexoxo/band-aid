404ef2f1dae99a6fac298a12bc5e4c9c1f6f2277
Sofie Cai <sofie.cai@ucll.be>
2024-04-25 16:05:44 +0200

    make functions of it

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,39 +3,51 @@ import type { paths } from "../lib/api/v1";
 
 const client = createClient<paths>({ baseUrl: "/" });
 
-const getBands = await client.GET("/api/bands");
+const getBands = async () => {
+    return await client.GET("/api/bands");
+}
 
-const postToken = await client.POST("/api/token", {
-    params: {
-        query: { dsn: "dsn" },
-    },
-    body: {
-        grant_type: "",
-        username: "username",
-        password: "password",
-        scope: "",
-        client_id: "",
-        client_secret: "",
-    }
-});
-const postRegister = await client.POST("/api/register", {
-    params: {
-        query: { dsn: "dsn" },
-    },
-    body: {
-        email: "string",
-        password: "string",
-    },
-});
+const getToken = async (dsn: string, grant_type: string, username: string, password: string, scope: string, client_id: string, client_secret: string) => {
+    return client.POST("/api/token", {
+        params: {
+            query: { dsn: dsn },
+        },
+        body: {
+            grant_type: grant_type,
+            username: username,
+            password: password,
+            scope: scope,
+            client_id: client_id,
+            client_secret: client_secret,
+        }
+    });
+};
+const registerUser = async (dsn: string, email: string, password: string) => {
+    return client.POST("/api/register", {
+        params: {
+            query: { dsn: dsn },
+        },
+        body: {
+            email: email,
+            password: password,
+        },
+    });
+};
 
-const postLogout = await client.POST("/api/logout");
+
+const logoutUser = async () => {
+    return client.POST("/api/logout");
+}
 
 
-const postBook = await client.POST("/api/book", {
-    body: {
-        name: "name",
-        email: "string",
-        date: "date",
-        band: "band",
-    },
-});
+const bookBand = async (name: string, email: string, date: string, band: string) => {
+    return client.POST("/api/book", {
+        body: {
+            name: name,
+            email: email,
+            date: date,
+            band: band,
+        },
+    });
+};
+
